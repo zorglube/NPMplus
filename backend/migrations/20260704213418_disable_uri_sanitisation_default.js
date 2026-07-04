0f04b84b1c6f21895771cb53f7fd9f3c66a355ae
Zoey <zoey@z0ey.de>
2026-07-04 20:31:28 +0200

    move aio.sh to setup.js

---
+++ backend/migrations/20260704213418_disable_uri_sanitisation_default.js
@@ -0,0 +1,36 @@
+import { migrate as logger } from "../logger.js";
+
+const migrateName = "disable_uri_sanitisation_default";
+
+/**
+ * Migrate
+ *
+ * @see https://knexjs.org/guide/migrations.html#migration-api
+ *
+ * @param   {Object} knex
+ * @returns {Promise}
+ */
+const up = (knex) => {
+	logger.info(`[${migrateName}] Migrating Up...`);
+
+	return knex.schema
+		.table("proxy_host", (proxy_host) => {
+			proxy_host.integer("npmplus_disable_uri_sanitisation").notNull().unsigned().defaultTo(0).alter();
+		})
+		.then(() => {
+			logger.info(`[${migrateName}] proxy_host Table altered`);
+		});
+};
+
+/**
+ * Undo Migrate
+ *
+ * @param   {Object} knex
+ * @returns {Promise}
+ */
+const down = (_knex) => {
+	logger.warn(`[${migrateName}] You can't migrate down this one.`);
+	return Promise.resolve(true);
+};
+
+export { up, down };
